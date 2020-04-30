9d9a74f352d18c84fd2a930aeab00a419a2c8e78
cyriella <58207431+cyriella@users.noreply.github.com>
2020-04-30 20:42:41 +0100

    Phase 2

---
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -0,0 +1,31 @@
+package jpu2016.dogfight.model;
+
+
+
+import java.util.ArrayList;
+
+
+public interface IDogfightModel {
+	
+	public IArea getIArea();
+
+	public void buildArea(Dimension dimension);
+	
+	public void addMobile(IMobile Mibile);
+	
+	public void removeMobile(IMobile Mobile);
+	
+	public ArrayList<IMobile> getMobiles();
+	
+	public IMobile getMobileByPlayer(int player);
+
+	public void setMobilesHavesMoved();
+
+	
+
+}
+
+	
+
+
+
